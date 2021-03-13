72c8724d211eb2e6e8c89ce481c303f4b06856b4
joe-park <parkjs3@yahoo.com>
2021-03-12 16:33:01 -0800

    mvp complete

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,40 @@
 // write your custom hook here to control your checkout form
+import { useEffect, useState } from "react";
+import { schema } from "../schema";
+
+export const useForm = (state) => {
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [values, setValues] = useState(state);
+  const [disabled, setDisabled] = useState(true);
+  let initialErrors = {};
+  for (let prop in state) {
+    initialErrors[prop] = "";
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let valid = await schema.isValid(values);
+    if (valid) {
+      setShowSuccessMessage(true);
+    }
+  };
+
+  useEffect(() => {
+    schema
+      .isValid(values)
+      .then((valid) => {
+        if (valid) {
+          setDisabled(false);
+        } else if (!valid) {
+          setDisabled(true);
+        }
+      })
+      .catch((e) => console.log("e: ", e));
+  }, [values]);
+
+  const handleChanges = async (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  return [values, handleChanges, handleSubmit, showSuccessMessage, disabled];
+};
